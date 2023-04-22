1a661f5e0f91524c2484b033d3040006fc588c5f
YOUSSEF EL BEQQAL <elbeqqal.youssef@gmail.com>
2023-04-22 01:51:27 +0000

    finish demo app

---
+++ client.js
@@ -0,0 +1,30 @@
+// Packages
+const grpc = require("grpc");
+const protoLoader = require("@grpc/proto-loader");
+
+// load proto file
+const packageDef = protoLoader.loadSync("todo.proto", {});
+
+// load package definition
+const grpcObject = grpc.loadPackageDefinition(packageDef);
+
+// get todoPackage
+const todoPackage = grpcObject.todoPackage;
+
+// setup client
+const client = new todoPackage.Todo("localhost:4000", grpc.credentials.createInsecure());
+
+// client will call createTodo method
+client.createTodo({
+    "id": 2,
+    "text": "learning grpc"
+}, (error, response) => {
+    if(error) console.log(`Ooops internal server error: ${error}`)
+    console.log(`(createTodo) Recieved from server: ${JSON.stringify(response)}`);
+});
+
+// client will readTodos method
+client.readTodos({}, (error, response) => {
+    if(error) console.log(`Ooops internal server error: ${error}`)
+    console.log(`(readTodos) Recieved from server: ${JSON.stringify(response)}`);
+});
---
+++ server.js
@@ -0,0 +1,40 @@
+// Packages
+const grpc = require("grpc");
+const protoLoader = require("@grpc/proto-loader");
+
+// load proto file
+const packageDef = protoLoader.loadSync("todo.proto", {});
+
+// load package definition
+const grpcObject = grpc.loadPackageDefinition(packageDef);
+
+// get todoPackage
+const todoPackage = grpcObject.todoPackage;
+
+// create server
+const server = new grpc.Server();
+
+server.bind("0.0.0.0:4000", grpc.ServerCredentials.createInsecure());
+
+// add service
+server.addService(todoPackage.Todo.service, {
+    "createTodo": createTodoController,
+    "readTodos": readTodosController
+});
+
+server.start();
+
+const todos = [];
+
+function createTodoController(call, callback) {
+    const todoItem = {
+        "id": todos.length + 1,
+        "text": call.request.text
+    }
+    todos.push(todoItem);
+    callback(null, todoItem);
+};
+
+function readTodosController(call, callback) {
+    callback(null, {"items": todos});
+};
